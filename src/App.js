99e51efb814ac2b474be113063aaa536b406b5a3
ajitha99 <ajitha.anandapu@gmail.com>
2022-03-17 10:32:23 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,79 @@
 import { useState } from "react";
 import './App.css';
 import './styles/style.css'
-import {receipt1,receipt2,receipt3} from './Data';
+// import {receipt1,receipt2,receipt3} from './Data';
+import receipts from './Data'
 import Receipt from "./components/Receipt";
 
 function App() {
-  const [receipts, setReceipts] = useState({receipt1,receipt2,receipt3});
   
-  // const receip = receipts.receipt1;
+  // const [allReceipts, setReceipts] = useState({receipt1,receipt2,receipt3});
+  const [allReceipts, setReceipts] = useState(receipts);
+
+  const[paidStatus, setPaidStatus] = useState(false);
+
+  // const changePaidStatus = (event) =>{
+  //   const isPaid = event.target.value;
+  //     setPaidStatus(isPaid);
+  // };
+  const changePaidStatus = () =>{
+    // setPaidStatus(!paidStatus);
+      setPaidStatus(true);
+  }
+
+
+  
+  
+  
   return (
     <div className="App">
-      <div><Receipt 
-       receipt = {receipts.receipt1}
-      /></div>
-      <div><Receipt 
-       receipt = {receipts.receipt2}
-      /></div>
+     
+       {
+       allReceipts.map((receipt, index) =>{
+          
+           return <Receipt 
+           receipt = { receipt}
+           paidStatus ={paidStatus}
+           changePaidStatus = {changePaidStatus}
+           key={ index} />
+           
+         })
+      }
+         
+         {/* const receip = receipts.receipt1; */}
+      {/* <div><Receipt 
+        receipt = {allReceipts.receipt1}
+        /></div>
+      <div><Receipt receipt = {allReceipts.receipt2}
+        /></div>
       <div> <Receipt 
-       receipt = {receipts.receipt3}
-      /></div>
-      {/* <h1>Receipt: {receipt1}</h1> */}
+       receipt = {allReceipts.receipt3}
+      /></div> */}
       
+          
     </div>
-  );
-}
+
+  )}
 
 export default App;
 
 
+ {/* allReceipts.map((receipt, index) =>{
+           return <Receipt 
+           receipt = { receipt}
+           key={ index} />
+         }) */}
 
 
 
-      // {  receipt1.map((receiptData , index) =>{
-      //   return <Receipt receipt = { receiptData} key = { index} />
-      //   })
-      // }
+
+{/* <h1>Receipt: {receipt1}</h1> */}
+
+
+
+// const [editPaid, setEditPaid] = useState(false);
+// const toggleEditPaidStatus = (event) => {
+//   const receiptPaid = event.target.paid;
+//   setEditPaid(receiptPaid);
+// }
+
--- src/components/Receipt.js
+++ src/components/Receipt.js
@@ -1,7 +1,32 @@
 
 function Receipt (props){
+
     return(
-        <div className="divClass">
+        <div>
+        { !props.receipt.paid ? 
+            
+            <div className="divClass" onClick={props.changePaidStatus} >
+                <h1>{props.receipt.person}</h1>
+                <h3><span>Main: </span>{props.receipt.order.main}</h3>
+                <h3><span>Protein: </span>{props.receipt.order.protein}</h3>
+                <h3><span>Rice: </span>{props.receipt.order.rice}</h3>
+                <h3><span>Sauce: </span> {props.receipt.order.sauce}</h3>
+                <h3><span>Drink: </span>{props.receipt.order.drink}</h3>
+                <h3><span>Cost: </span>{props.receipt.order.cost}</h3>
+                <h3><span>Paid: </span>{props.receipt.paid = props.paidStatus}</h3>
+                {/* <h3>{!props.receipt.paid}? {props.paidStatus}: return</h3> */}  
+                </div> 
+                : null}
+        </div>
+        
+    )
+}
+export default Receipt;
+
+{/* <div>
+        { !props.receipt.paid ? 
+            
+        <div className="divClass"> 
             <h1>{props.receipt.person}</h1>
             <h3><span>Main: </span>{props.receipt.order.main}</h3>
             <h3><span>Protein: </span>{props.receipt.order.protein}</h3>
@@ -9,8 +34,7 @@ function Receipt (props){
             <h3><span>Sauce: </span> {props.receipt.order.sauce}</h3>
             <h3><span>Drink: </span>{props.receipt.order.drink}</h3>
             <h3><span>Cost: </span>{props.receipt.order.cost}</h3>
-        </div>
-    )
-}
-export default Receipt;
+            </div> 
+            : null}
+        </div> */}
 
